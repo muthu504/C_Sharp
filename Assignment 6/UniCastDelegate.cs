e728b501450bc2c16ea5a74ca2d2caa7bc3dc84d
muthu504 <102361374+muthu504@users.noreply.github.com>
2022-04-20 18:59:15 +0530

    Update UniCastDelegate.cs

--- Assignment 6/UniCastDelegate.cs
+++ Assignment 6/UniCastDelegate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment6
+namespace deleemployee
 { /*UniCast Delegate*/
     public delegate void EmployeeDelegate();
     public class UniCastDelegate
