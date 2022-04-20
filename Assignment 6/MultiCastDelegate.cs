db918ce86c275b1a88a199b104e7023daeb057f7
muthu504 <102361374+muthu504@users.noreply.github.com>
2022-04-20 18:56:23 +0530

    Update MultiCastDelegate.cs

--- Assignment 6/MultiCastDelegate.cs
+++ Assignment 6/MultiCastDelegate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment6
+namespace multicastDelegate
 { /*Multicaste Delegate*/
     public delegate void EmployeeDelegate();
     public class MultiCastDelegate
