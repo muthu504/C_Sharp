6a4e6ce37607fd664f3b5a01d60e7cba205d846e
muthu504 <102361374+muthu504@users.noreply.github.com>
2022-04-20 18:39:14 +0530

    Update EmpLinkedList.cs

--- Assignment 5/EmpLinkedList.cs
+++ Assignment 5/EmpLinkedList.cs
@@ -13,19 +13,19 @@ namespace LinkedListForEmployees
             Console.WriteLine("Program starts...");
             LinkedList<String> list = new LinkedList<String>();
             Console.WriteLine("Adding Employee Details");
-            list.AddFirst("Bhargav"); //a) adding a new employee
-            list.AddLast("Ram Charan");   //a) adding a new employee
-            list.AddLast("NTR");  //a) adding a new employee
-            list.AddLast("Rahul");   //a) adding a new employee
-            list.AddLast("Virat");   //a) adding a new employee
+            list.AddFirst("Vasanth"); //a) adding a new employee
+            list.AddLast("Bindhu");   //a) adding a new employee
+            list.AddLast("Vagdevi");  //a) adding a new employee
+            list.AddLast("chitti");   //a) adding a new employee
+            list.AddLast("Tarun");   //a) adding a new employee
 
             foreach (String item in list)   //b) displaying a list of employees using foreach loop
             {
                 Console.WriteLine("We Overwhelmingly welcome newly Joiners : {0} ",item);    
             }
             Console.WriteLine("===============================================================");
-            Console.WriteLine(list.Contains("Ram Charan"));   //performing specific search operation (true/false)
-            Console.WriteLine(list.Contains("Bhargav"));  //performing specific search operation (true/false)
+            Console.WriteLine(list.Contains("Vasanth"));   //performing specific search operation (true/false)
+            Console.WriteLine(list.Contains("Bindhu"));  //performing specific search operation (true/false)
             Console.WriteLine("===============================================================");
             //c)total number of employees in the list/Count of employees
             Console.WriteLine("Total number of employees in the linked list is : {0} ", list.Count); 
